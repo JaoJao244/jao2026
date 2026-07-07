1bfbd78c58b5aa23ceca87b9b6f197154b569163
JaoJao244 <rangel.andrade@escola.pr.gov.br>
2026-07-07 09:24:00 -0300

    o.

--- mains.js
+++ mains.js
@@ -1,6 +1,4 @@
-for (let i = 0; i < tempos.length; i++) {
-  document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
-}for (let i = 0; i < contadores.length; i++) {
+for (let i = 0; i < contadores.length; i++) {
   document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
   document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
   document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
